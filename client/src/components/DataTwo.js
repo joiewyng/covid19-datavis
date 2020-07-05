d40fb863b45e29966b03cc83299ca29ddc3f6f64
Joie Ng <joie.wy.ng@gmail.com>
2020-07-05 18:24:59 -0400

    energy bar chart checkbox filter

--- client/src/components/DataTwo.js
+++ client/src/components/DataTwo.js
@@ -9,7 +9,8 @@ import {
     VictoryGroup,
     VictoryLabel,
     VictoryTooltip,
-    VictoryLegend
+    VictoryLegend,
+    VictoryVoronoiContainer
    } from 'victory';
 export default class DataTwo extends React.Component {
 
@@ -149,16 +150,9 @@ class DonutChart extends React.Component {
                 <strong style={{lineHeight: 3, fontSize: 20}}>Net Generation from Renewable Sources</strong>
                 <div>MWh = Megawatthours</div>
                 <select style={{margin: 15, padding:5}}value={this.state.year} onChange={this.handleChange}>
-                    <option value="0">2010</option>
-                    <option value="1">2011</option>
-                    <option value="2">2012</option>
-                    <option value="3">2013</option>
-                    <option value="4">2014</option>
-                    <option value="5">2015</option>
-                    <option value="6">2016</option>
-                    <option value="7">2017</option>
-                    <option value="8">2018</option>
-                    <option value="9">2019</option>
+                    {[0,1,2,3,4,5,6,7,8,9].map((num, index) => {
+                        return <option value={num} key={index}>201{num}</option>
+                    })}
              </select>
             </div>
             
@@ -186,7 +180,7 @@ class DonutChart extends React.Component {
 }
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-const energyType = ["wind", "solarPhotovoltaic", "woodFuels", "hydroelectric"];
+const energyTypes = ['hydroelectric', 'wind', 'solarPhotovoltaic', 'woodFuels'];
 class HorizBarChart extends React.Component {
     constructor(props) {
         super(props);
@@ -195,19 +189,23 @@ class HorizBarChart extends React.Component {
             solarPhotovoltaic: [],
             woodFuels: [],
             hydroelectric: [],
-            loading: true
+            loading: true,
+            splitByType: true,
         }
+        this.handleCheckboxInputChange = this.handleCheckboxInputChange.bind(this);
     }
     configBarData(){
         let wind = [];
         let solar = [];
         let wood = [];
         let hydro = [];
+        console.log(this.props.data);
         for (let i = 0; i < this.props.data.length; i++){
             wind.push(parseFloat(this.props.data[i]["Wind"].replace(/,/g, '')));
             solar.push(parseFloat(this.props.data[i]["Solar Photovoltaic"].replace(/,/g, '')));
             wood.push(parseFloat(this.props.data[i]["Wood and Wood-Derived Fuels"].replace(/,/g, '')));
             hydro.push(parseFloat(this.props.data[i]["Conventional Hydroelectric"].replace(/,/g, '')));
+
         }
         this.setState({
             wind: wind,
@@ -217,17 +215,63 @@ class HorizBarChart extends React.Component {
         })
     }
 
+    monthlySumData(){
+        let sums = [];
+        let types = ['Wind', 'Solar Photovoltaic', 'Wood and Wood-Derived Fuels', 'Conventional Hydroelectric'];
+        console.log(this.props.data[0]['Wind']);
+        for (let i = 0; i < this.props.data.length; i++){
+            let sum = types.reduce((acc, val) => {
+   
+                return acc + parseFloat(this.props.data[i][val].replace(/,/g, ''));
+            }, 0)
+            sums.push({
+                x: months[i],
+                y: sum,
+            });
+        }
+        console.log(sums);
+       return sums;
+    }
+
+    categoryLabel(category){
+        let label = '';
+        switch (category){
+            case 'wind':
+                label = 'Wind'
+                break;
+            case 'solarPhotovoltaic':
+                label = 'Solar Photovoltaic';
+                break;
+            case 'woodFuels':
+                label = 'Wood Fuels';
+                break;
+            case 'hydroelectric':
+                label = 'Hydroelectric';
+        }
+        return label;
+    }
+
     configCategoryData(category){
         let data = [];
         for (let i = 0; i < 12; i++){
             data.push(
                 {
                     x: months[i],
-                    y:this.state[category][i]
+                    y:this.state[category][i],
+                    category: this.categoryLabel(category),
                 }
             )
         }
         return data;
+    }
+
+    handleCheckboxInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.checked;
+        this.setState({
+            [name]: value,
+        })
     }
 
     componentDidMount(){
@@ -246,47 +290,81 @@ class HorizBarChart extends React.Component {
             <>
             <strong style={{lineHeight: 3, fontSize: 20}}>Net Generation from Top Four Renewable Sources in 2018</strong>
             <div>in Thousand Megawatthours</div>
+            <label>
+            <input
+                style={{margin: 10}}
+                name="splitByType"
+                type="checkbox"
+                checked={this.state.splitByType}
+                onChange={this.handleCheckboxInputChange} />
+                Split by energy type
+            </label>
             <div style={{marginLeft: "30%", marginRight: "30%"}}>
                 <VictoryChart
                 // theme={VictoryTheme.material}
                 domainPadding={{ y: 12, x: 12 }}
                 domain={{ y: [0.5, 100000] }}
-                >
-                      <VictoryLegend x={300} y={50}
-                        title="Energy Sources"
-                        centerTitle
-                        orientation="vertical"
-                        gutter={15}
-                        style={{ border: { stroke: "black" }, title: {fontSize: 10 } }}
-                        data={[
-                        { name: "Wind", symbol: { fill: "brown"} },
-                        { name: "Hydroelectric", symbol: { fill: "gold" } },
-                        { name: "Solar", symbol: { fill: "tomato" } },
-                        { name: "Wood Fuels", symbol: { fill: "orange" } },
-                        ]}
+                containerComponent={
+                    <VictoryVoronoiContainer
+                      style={{padding: 10}}
+                      labels={
+                        this.state.splitByType ? (
+                            ({ datum }) => datum.y > 0 ? `${datum.category} \n ${datum.y} MWh` : null) 
+                        : (({ datum }) => datum.y > 0 ? `  ${datum.y} MWh` : null) 
+                      }
+                         
+                        
                     />
+                }
+            >
+                    {
+                        this.state.splitByType ? (
+                            <VictoryLegend x={300} y={50}
+                                title="Energy Sources"
+                                centerTitle
+                                orientation="vertical"
+                                gutter={15}
+                                style={{ border: { stroke: "black" }, title: {fontSize: 10 } }}
+                                data={[
+                                { name: "Hydroelectric", symbol: { fill: "brown" } },
+                                { name: "Wind", symbol: { fill: "gold"} },
+                                { name: "Solar", symbol: { fill: "tomato" } },
+                                { name: "Wood Fuels", symbol: { fill: "orange" } },
+                                ]}
+                            />
+                        ) : (
+                            <VictoryLegend x={300} y={50}
+                                title="Energy Sources"
+                                centerTitle
+                                orientation="vertical"
+                                gutter={15}
+                                style={{ border: { stroke: "black" }, title: {fontSize: 10 } }}
+                                data={[
+                                { name: "Hydroelectric", symbol: { fill: "white" } },
+                                { name: "Wind", symbol: { fill: "white" } },
+                                { name: "Solar", symbol: { fill: "white" } },
+                                { name: "Wood Fuels", symbol: { fill: "white" } },
+                                ]}
+                            />
+                        )
+                    }
+                      
                     <VictoryGroup horizontal
                     offset={10}
                     style={{ data: { width: 13 } }}
                     colorScale={["brown", "gold", "orange", "tomato"]}
                     >
                     <VictoryStack>
-                        <VictoryBar
-                            data={this.configCategoryData("wind")}
-                      
-                        />
-                        <VictoryBar
-                            data={this.configCategoryData("hydroelectric")}
-                        />
-                        <VictoryBar
-                            data={this.configCategoryData("solarPhotovoltaic")}
-                        />
-                        <VictoryBar
-                            data={this.configCategoryData("woodFuels")}
-                        />
-                         
+                        
+                        {
+                            this.state.splitByType ? (
+                                energyTypes.map((energyType, index) => {
+                                    return <VictoryBar key={index} data={this.configCategoryData(energyType)}/>
+                                })
+                            ) : (<VictoryBar data={this.monthlySumData()} colorScale={['#8DC3A7']}/>)
+
+                        }
                     </VictoryStack>
-                    
                 </VictoryGroup>
                 </VictoryChart>
             </div>
@@ -299,7 +377,6 @@ class PieChart extends React.Component {
     constructor(props){
         super(props);
     }
-
     render(){
         return (
             <>
